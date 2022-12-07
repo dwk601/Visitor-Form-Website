9e2fd3d2813128c8ddc085a4051352caf49dae07
dwk601 <108056780+dwk601@users.noreply.github.com>
2022-12-07 16:23:48 -0700

    updated main.js

--- main.js
+++ main.js
@@ -1,31 +1,18 @@
-// take a log of the personal information of the user
-// and store it in the database
-// this is the main function of the app
-function logPersonalInfo() {
-  // get the personal information of the user
-  var personalInfo = getPersonalInfo();
-  // store the personal information in the database
-  storePersonalInfo(personalInfo);
-}
+// when submitBtn is clicked, submit the form
+document.getElementById("visitorForm").submit();
 
-// Path: main.js
-// get the personal information of the user
-function getPersonalInfo() {
-  // get the name of the user
-  var name = getName();
-  // get the address of the user
-  var address = getAddress();
-  // get the phone number of the user
-  var phoneNumber = getPhoneNumber();
-  // get the email of the user
-  var email = getEmail();
-  // return the personal information of the user
-  return {
-    name: name,
-    address: address,
-    phoneNumber: phoneNumber,
-    email: email,
-  };
-}
+// when submitBtn is clicked, save the value of the input field to localStorage
+// then, when logBtn is clicked, log the value of the input field to the console
+const submitBtn = document.querySelector("#submitBtn");
+const logBtn = document.querySelector("#logBtn");
+const input = document.querySelector("#input");
 
-console.log(getPersonalInfo());
+submitBtn.addEventListener("click", () => {
+  localStorage.setItem("input", input.value);
+});
+
+logBtn.addEventListener("click", () => {
+  console.log(localStorage.getItem("input"));
+});
+
+// Path: index.html
